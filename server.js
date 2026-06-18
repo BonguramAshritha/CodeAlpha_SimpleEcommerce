0545008890422f817623a4186f4df345130ff21e
BonguramAshritha <bonguramashritha@gmail.com>
2026-06-18 10:52:59 +0530

    CodeAlpha Task 1 - SnapKart E-Commerce Store

--- server.js
+++ server.js
@@ -20,21 +20,21 @@ app.get("/product/:name", (req, res) => {
     const products = {
         watch: {
             name: "Smart Watch",
-            price: "₹1999",
+            price: 1999,
             image: "https://m.media-amazon.com/images/I/61ZjlBOp+rL._SX679_.jpg",
             description: "Stylish smart watch with fitness tracking."
         },
 
         headphones: {
             name: "Headphones",
-            price: "₹1499",
+            price: 1499,
             image: "https://m.media-amazon.com/images/I/61CGHv6kmWL._SX679_.jpg",
             description: "Gaming headphones with great sound quality."
         },
 
         shoes: {
             name: "Shoes",
-            price: "₹2499",
+            price: 2499,
             image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400",
             description: "Comfortable running shoes."
         }
